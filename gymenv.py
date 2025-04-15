542886b6cee0578a6b21c6758a2fd531c8d6ce9e
Jordan Lassers <lassersj@yahoo.com>
2025-04-14 18:19:01 -0700

    Updated model

--- gymenv.py
+++ gymenv.py
@@ -1,5 +1,6 @@
 # Todo
-# Punish when jumping when not near ledge
+# Reward for killing / damaging enemies
+# Penalty for falling in water
 
 
 import numpy as np
@@ -81,8 +82,9 @@ class ShooterEnv(gym.Env):
         self.prev_x = self.start_x  # Track previous horizontal position
         self.stuck_counter = 0 # Counter to determine amount of frames an agent is stuck for
         self.furthest_x = self.game.player.rect.centerx # Variable to keep track of record x distance
-        self.reached_checkpoints = set() #Set to keep track of checkpoints that have been reached
-
+        
+        self.direction_history = []  # store last 10 movement directions
+        self.max_direction_history = 10  # size of buffer
 
 
 
@@ -108,8 +110,22 @@ class ShooterEnv(gym.Env):
         observation, debug_info = self._get_observation()
         self.last_action = action
         reward = self._get_reward()
-        terminated = not self.game.player.alive or self.game.level_complete or self.stuck_counter >= 300
-        truncated = self.step_count >= 2000
+        
+        # Track last movement direction
+        if action == 0:
+            self.direction_history.append(-1)  # left
+        elif action == 1:
+            self.direction_history.append(1)   # right
+        else:
+            self.direction_history.append(0)   # no movement
+        
+        # Keep buffer size in bounds
+        if len(self.direction_history) > self.max_direction_history:
+            self.direction_history.pop(0)
+
+
+        terminated = not self.game.player.alive or self.game.level_complete
+        truncated = self.step_count >= 1000
 
         return observation, reward, terminated, truncated, debug_info
 
@@ -203,47 +219,39 @@ class ShooterEnv(gym.Env):
         if not p.alive: # Punishment for dying
             return -300
 
-        player_x = p.rect.centerx
-
-        # Reward for distance travelled
-        if player_x > self.furthest_x:
-            distance_reward = min((player_x - self.furthest_x) * 0.01, 10)
-            reward += distance_reward
-            self.furthest_x = player_x
-        
-        # Reward for reaching checkpoint
-        checkpoint_size = 200
-        checkpoint = player_x // checkpoint_size
-        if checkpoint not in self.reached_checkpoints:
-            reward += 50
-            self.reached_checkpoints.add(checkpoint)
+        delta_x = p.rect.centerx - self.prev_x
+        if delta_x > 0:
+            reward += 1.5
+            self.furthest_x = p.rect.centerx
+        elif delta_x < 0:
+            reward += .05
         
         # Track if agent is stuck and not moving right or left
-        if player_x == self.prev_x:
+        if p.rect.centerx == self.prev_x:
             self.stuck_counter += 1
         else:
             self.stuck_counter = 0
         
-        # Penalize if stuck too long > 100 frames
-        if self.stuck_counter >= 100:
-            reward -= 10
+        # # Penalize if stuck too long > 100 frames
+        # if self.stuck_counter >= 100:
+        #     reward -= 10
         
         # Bonus reward for beating level
         if self.game.level_complete:
-            reward += 1000
+            reward += 300
             
-        # penalty for using ammo when empty    
-        if action == 5:
-            if self._able_to_shoot_enemy(p):
-                reward += 5  # Encourage shooting when in range of enemy
-            elif p.ammo == 0:
-                reward -= 0.5  # lessor penalty
-            else:
-                reward -= 0.2  # prevent spam
+        # # penalty for using ammo when empty    
+        # if action == 5:
+        #     if self._able_to_shoot_enemy(p):
+        #         reward += 5  # Encourage shooting when in range of enemy
+        #     elif p.ammo == 0:
+        #         reward -= 0.5  # lessor penalty
+        #     else:
+        #         reward -= 0.2  # prevent spam
         
-        # Penalty for jump spamming
-        if action == 2 and p.in_air:
-            reward -= 1
+        # # Penalty for jump spamming
+        # if action == 2 and p.in_air:
+        #     reward -= 1
 
         if self.game.facing_death_gap(p) and action == 4:  # jump + right, reward jumping over gaps
             reward += 10  
@@ -265,21 +273,31 @@ class ShooterEnv(gym.Env):
         elif p.health < self.prev_health:
             reward -= 10  # Took damage
             
-        if action == 5 and self._able_to_shoot_enemy(p):
-            reward += 3  #Meant to reward idea of shooting at enemy even if out of ammo    
+        # if action == 5 and self._able_to_shoot_enemy(p):
+        #     reward += 3  #Meant to reward idea of shooting at enemy even if out of ammo    
             
-        # Reward for damaging enemies
-        for enemy in self.game.groups['enemy']:
-            enemy_id = id(enemy)
-            prev_health = self.prev_enemy_health.get(enemy_id, enemy.health)
-            damage = prev_health - enemy.health
-            if damage > 0:
-                reward += damage * 1  
-            # Reward for killing enemy
-            if prev_health > 0 and enemy.health <= 0:
-                reward += 50
+        # # Reward for damaging enemies
+        # for enemy in self.game.groups['enemy']:
+        #     enemy_id = id(enemy)
+        #     prev_health = self.prev_enemy_health.get(enemy_id, enemy.health)
+        #     damage = prev_health - enemy.health
+        #     if damage > 0:
+        #         reward += damage * 1  
+        #     # Reward for killing enemy
+        #     if prev_health > 0 and enemy.health <= 0:
+        #         reward += 50
                 
-            self.prev_enemy_health[enemy_id] = enemy.health
+        #     self.prev_enemy_health[enemy_id] = enemy.health
+            
+            
+        # # Penalize spamming left and right
+        # if len(self.direction_history) == self.max_direction_history:
+        #     direction_changes = sum(
+        #         abs(self.direction_history[i] - self.direction_history[i-1]) == 2
+        #         for i in range(1, len(self.direction_history))
+        #     )
+        #     if direction_changes >= 4:  # tweak this threshold
+        #         reward -= 3  # apply penalty
 
         # Update trackers
         self.prev_ammo = p.ammo
@@ -344,8 +362,8 @@ class ShooterEnv(gym.Env):
             same_direction = (
                 (player.direction == 1 and enemy_rect.centerx > player_rect.centerx) or (player.direction == -1 and enemy_rect.centerx < player_rect.centerx))
     
-            #Check if the enemy is within range of 800 pixels
-            in_range = abs(enemy_rect.centerx - player_rect.centerx) < 800
+            #Check if the enemy is within range of 500 pixels
+            in_range = abs(enemy_rect.centerx - player_rect.centerx) < 500
     
             if same_y and same_direction and in_range:
                 return 1
